a8aba0f49795fa349b4168f4d05eacb79ed3b0bd
joeyamyers <joeyandmyers@gmail.com>
2022-03-23 11:53:17 -0700

    began mapping geojson data

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,63 @@
+// boilerplate
+
+// Create our initial map object and Set the longitude, latitude, and the starting zoom level
+var myMap = L.map("map", {
+    center: [37.5, -122.5],
+    zoom: 10
+  });
+
+  // Map a GeoJSON Point - this GeoData is a featurecollection obj
+// Add GeoJSON data - coordinates re backwards for geojson data (lng, lat)
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// L.geoJSON(sanFranAirport).addTo(myMap);
+  
+// // to add functionality to a marker use the pointToLayer() func
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3> " + feature.properties.city + ", " + feature.properties.country + "</h3>");
+//   }
+
+// }).addTo(myMap);
+
+  // onEachFeature function
+//   L.geoJSON(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup();
+//      }
+// }).addTo(myMap);
+  
+
+  // Add a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: 'mapbox/satellite-streets-v11',
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+
